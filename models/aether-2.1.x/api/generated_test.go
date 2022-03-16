5d73db25b7fed9748c9f96442f9f02d109e5e1cd
Sean Condon <sean@opennetworking.org>
2022-03-16 17:37:22 +0000

    Adding new 2.1.x model (#26)

---
+++ models/aether-2.1.x/api/generated_test.go
@@ -0,0 +1,215 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package api
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// aStr facilitates easy declaring of pointers to strings
+func aStr(s string) *string {
+	return &s
+}
+
+func TestApplicationAddress(t *testing.T) {
+	app := OnfEnterprise_Enterprises_Enterprise_Application{}
+
+	app.Address = aStr("my.host.name")
+	err := app.Validate()
+	assert.Nil(t, err)
+
+	app.Address = aStr("1.2.3.4")
+	err = app.Validate()
+	assert.Nil(t, err)
+
+	app.Address = aStr("1.2.3.4/1")
+	err = app.Validate()
+	assert.Nil(t, err)
+
+	app.Address = aStr("1.2.3.4/32")
+	err = app.Validate()
+	assert.Nil(t, err)
+
+	// Error - malformed ip address
+	app.Address = aStr("1..2.3.4")
+	err = app.Validate()
+	assert.Error(t, err)
+
+	// Error - missing subnet bits
+	app.Address = aStr("1.2.3.4/")
+	err = app.Validate()
+	assert.Error(t, err)
+
+	// Error - subnet bits are too many
+	app.Address = aStr("1.2.3.4/33")
+	err = app.Validate()
+	assert.Error(t, err)
+
+	// Error - garbage in subnet section
+	app.Address = aStr("1.2.3.4/x33")
+	err = app.Validate()
+	assert.Error(t, err)
+
+	// Error - subnet not a number
+	app.Address = aStr("1.2.3.4/x")
+	err = app.Validate()
+	assert.Error(t, err)
+}
+
+func TestSimCard(t *testing.T) {
+	sim := OnfEnterprise_Enterprises_Enterprise_Site_SimCard{}
+
+	// 18 digit, starts with 0
+	sim.Iccid = aStr("023456789012345678F")
+	err := sim.Validate()
+	assert.Nil(t, err)
+
+	// 18 + check digit
+	sim.Iccid = aStr("123456789012345678F")
+	err = sim.Validate()
+	assert.Nil(t, err)
+
+	// 19 + check digit
+	sim.Iccid = aStr("1234567890123456789F")
+	err = sim.Validate()
+	assert.Nil(t, err)
+
+	// 20 + check digit
+	sim.Iccid = aStr("12345678901234567890F")
+	err = sim.Validate()
+	assert.Nil(t, err)
+
+	// 21 + check digit
+	sim.Iccid = aStr("123456789012345678901F")
+	err = sim.Validate()
+	assert.Nil(t, err)
+
+	// 17 + check digit -- too short
+	sim.Iccid = aStr("12345678901234567F")
+	err = sim.Validate()
+	assert.Error(t, err)
+
+	// 22 + check digit -- too long
+	sim.Iccid = aStr("12345678901234567F")
+	err = sim.Validate()
+	assert.Error(t, err)
+
+	// invalid check digit
+	sim.Iccid = aStr("123456789012345678G")
+	err = sim.Validate()
+	assert.Error(t, err)
+
+	// invalid digit
+	sim.Iccid = aStr("1234567890123456A8F")
+	err = sim.Validate()
+	assert.Error(t, err)
+}
+
+func TestDevice(t *testing.T) {
+	dev := OnfEnterprise_Enterprises_Enterprise_Site_Device{}
+
+	// 14 digit, starts with 0
+	dev.Imei = aStr("02345678901234")
+	err := dev.Validate()
+	assert.Nil(t, err)
+
+	// 14 digit
+	dev.Imei = aStr("12345678901234")
+	err = dev.Validate()
+	assert.Nil(t, err)
+
+	// 15 digit
+	dev.Imei = aStr("123456789012345")
+	err = dev.Validate()
+	assert.Nil(t, err)
+
+	// 16 digit
+	dev.Imei = aStr("1234567890123456")
+	err = dev.Validate()
+	assert.Nil(t, err)
+
+	// 13 digit - too short
+	dev.Imei = aStr("123456789012343")
+	err = dev.Validate()
+	assert.Nil(t, err)
+
+	// 17 digit - too long
+	dev.Imei = aStr("12345678901234567")
+	err = dev.Validate()
+	assert.Error(t, err)
+
+	// not a valid digit
+	dev.Imei = aStr("123456789012345A")
+	err = dev.Validate()
+	assert.Error(t, err)
+}
+
+func TestIdentifier(t *testing.T) {
+	ent := OnfEnterprise_Enterprises_Enterprise{}
+
+	ent.EnterpriseId = aStr("test")
+	err := ent.Validate()
+	assert.Nil(t, err)
+
+	// Minimum length
+	ent.EnterpriseId = aStr("a")
+	err = ent.Validate()
+	assert.Nil(t, err)
+
+	// Maximum length
+	ent.EnterpriseId = aStr("a12345678901234567890123456789012345678901234567890123456789012")
+	err = ent.Validate()
+	assert.Nil(t, err)
+
+	ent.EnterpriseId = aStr("test-1")
+	err = ent.Validate()
+	assert.Nil(t, err)
+
+	// Two non-consecutive dashes are fine
+	ent.EnterpriseId = aStr("test-test-1")
+	err = ent.Validate()
+	assert.Nil(t, err)
+
+	// Two consecutive dashes are not allow
+	ent.EnterpriseId = aStr("test--1")
+	err = ent.Validate()
+	assert.Error(t, err)
+
+	// Uppercase is not allowed
+	ent.EnterpriseId = aStr("Uppercase")
+	err = ent.Validate()
+	assert.Error(t, err)
+
+	// Uppercase is not allowed, even if you hide it in the middle
+	ent.EnterpriseId = aStr("upperCase")
+	err = ent.Validate()
+	assert.Error(t, err)
+
+	// Starts with a number is not allowed
+	ent.EnterpriseId = aStr("1-test")
+	err = ent.Validate()
+	assert.Error(t, err)
+
+	// Empty string is not allowed
+	ent.EnterpriseId = aStr("")
+	err = ent.Validate()
+	assert.Error(t, err)
+
+	// Too Long
+	ent.EnterpriseId = aStr("a123456789012345678901234567890123456789012345678901234567890123")
+	err = ent.Validate()
+	assert.Error(t, err)
+
+	// No periods
+	ent.EnterpriseId = aStr("test.two")
+	err = ent.Validate()
+	assert.Error(t, err)
+
+	// No spaces
+	ent.EnterpriseId = aStr("test two")
+	err = ent.Validate()
+	assert.Error(t, err)
+}
---
+++ models/aether-2.1.x/api/must_test.go
@@ -0,0 +1,78 @@
+// SPDX-FileCopyrightText: 2019-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package api
+
+import (
+	"github.com/onosproject/config-models/pkg/xpath/navigator"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"testing"
+)
+
+func Test_WalkAndValidatePortsMust(t *testing.T) {
+	sampleConfig, err := ioutil.ReadFile("../testdata/sample-aether2-config.json")
+	if err != nil {
+		assert.NoError(t, err)
+	}
+	device := new(Device)
+
+	schema, err := Schema()
+	if err := schema.Unmarshal(sampleConfig, device); err != nil {
+		assert.NoError(t, err)
+	}
+	schema.Root = device
+	assert.NotNil(t, device)
+	nn := navigator.NewYangNodeNavigator(schema.RootSchema(), device)
+	assert.NotNil(t, nn)
+
+	ynn, ynnOk := nn.(*navigator.YangNodeNavigator)
+	assert.True(t, ynnOk)
+	validateErr := ynn.WalkAndValidateMust()
+	assert.NoError(t, validateErr)
+}
+
+func Test_WalkAndValidateMustPortsWrong(t *testing.T) {
+	sampleConfig, err := ioutil.ReadFile("../testdata/sample-must-port-start-end-wrong.json")
+	if err != nil {
+		assert.NoError(t, err)
+	}
+	device := new(Device)
+
+	schema, err := Schema()
+	if err := schema.Unmarshal(sampleConfig, device); err != nil {
+		assert.NoError(t, err)
+	}
+	schema.Root = device
+	assert.NotNil(t, device)
+	nn := navigator.NewYangNodeNavigator(schema.RootSchema(), device)
+	assert.NotNil(t, nn)
+
+	ynn, ynnOk := nn.(*navigator.YangNodeNavigator)
+	assert.True(t, ynnOk)
+	validateErr := ynn.WalkAndValidateMust()
+	assert.EqualError(t, validateErr, "port-start must be less than or equal to port-end. Must statement 'number(./ent:port-start) <= number(./ent:port-end)' to true. Container(s): [endpoint-id=da2]")
+}
+
+func Test_WalkAndValidateMustIpDomainSlice(t *testing.T) {
+	sampleConfig, err := ioutil.ReadFile("../testdata/sample-slice-dnn-notreuse.json")
+	if err != nil {
+		assert.NoError(t, err)
+	}
+	device := new(Device)
+
+	schema, err := Schema()
+	if err := schema.Unmarshal(sampleConfig, device); err != nil {
+		assert.NoError(t, err)
+	}
+	schema.Root = device
+	assert.NotNil(t, device)
+	nn := navigator.NewYangNodeNavigator(schema.RootSchema(), device)
+	assert.NotNil(t, nn)
+
+	ynn, ynnOk := nn.(*navigator.YangNodeNavigator)
+	assert.True(t, ynnOk)
+	validateErr := ynn.WalkAndValidateMust()
+	assert.EqualError(t, validateErr, `a device group cannot be used in more than one slice in a site. Must statement 'count(ent:slice[set-contains(following-sibling::ent:slice/ent:device-group/@ent:device-group, ent:device-group/@ent:device-group)]/@ent:slice-id) = 0' to true. Container(s): [slice-id=acme-dallas-robots]`)
+}
