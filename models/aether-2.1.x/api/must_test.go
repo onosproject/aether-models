d20f50c6b2f09446347b95c6a6c5bfbc271c5a48
Sean Condon <sean@opennetworking.org>
2022-04-04 22:13:09 +0100

    Aether-3393 move enterprise out to onos-topo (#31)
    * AETHER-3393 move enterprise out to onos-topo
    
    * remove top level objects
    
    * updating tests

--- models/aether-2.1.x/api/must_test.go
+++ models/aether-2.1.x/api/must_test.go
@@ -52,7 +52,7 @@ func Test_WalkAndValidateMustPortsWrong(t *testing.T) {
 	ynn, ynnOk := nn.(*navigator.YangNodeNavigator)
 	assert.True(t, ynnOk)
 	validateErr := ynn.WalkAndValidateMust()
-	assert.EqualError(t, validateErr, "port-start must be less than or equal to port-end. Must statement 'number(./ent:port-start) <= number(./ent:port-end)' to true. Container(s): [endpoint-id=da2]")
+	assert.EqualError(t, validateErr, "port-start must be less than or equal to port-end. Must statement 'number(./app:port-start) <= number(./app:port-end)' to true. Container(s): [endpoint-id=da2]")
 }
 
 func Test_WalkAndValidateMustIpDomainSlice(t *testing.T) {
@@ -74,5 +74,5 @@ func Test_WalkAndValidateMustIpDomainSlice(t *testing.T) {
 	ynn, ynnOk := nn.(*navigator.YangNodeNavigator)
 	assert.True(t, ynnOk)
 	validateErr := ynn.WalkAndValidateMust()
-	assert.EqualError(t, validateErr, `a device group cannot be used in more than one slice in a site. Must statement 'count(ent:slice[set-contains(following-sibling::ent:slice/ent:device-group/@ent:device-group, ent:device-group/@ent:device-group)]/@ent:slice-id) = 0' to true. Container(s): [slice-id=acme-dallas-robots]`)
+	assert.EqualError(t, validateErr, `a device group cannot be used in more than one slice in a site. Must statement 'count(site:slice[set-contains(following-sibling::site:slice/site:device-group/@site:device-group, site:device-group/@site:device-group)]/@site:slice-id) = 0' to true. Container(s): [slice-id=acme-dallas-robots]`)
 }
